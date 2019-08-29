88acb7b73b6e508e15d2a6330ee81fda8e2debf5
Aaron <ayybuddyy@gmail.com>
2019-08-28 22:26:37 -0400

    Add Likes view, model, controller, local storage
    Have liked recipes persist on local storage. Fix bugs.

--- src/js/index.js
+++ src/js/index.js
@@ -2,9 +2,11 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import List from './models/List';
+import Likes from './models/Likes';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
 import * as listView from './views/listView';
+import * as likesView from './views/likesView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
 // Global state of the app
@@ -64,7 +66,6 @@ elements.searchResPages.addEventListener('click', (e) => {
 const controlRecipe = async () => {
 	// Get ID from url
 	const id = window.location.hash.replace('#', '');
-	console.log(id);
 
 	if (id) {
 		// Prepare UI for changes
@@ -88,8 +89,10 @@ const controlRecipe = async () => {
 
 			// Render recipe
 			clearLoader();
-			recipeView.renderRecipe(state.recipe);
+			recipeView.renderRecipe(state.recipe, state.likes.isLiked(id));
 		} catch (err) {
+			console.log(err);
+
 			alert('Error processing recipe');
 		}
 	}
@@ -137,6 +140,47 @@ elements.shopping.addEventListener('click', (e) => {
  * LIKE CONTROLLER
  */
 
+const controlLike = () => {
+	if (!state.likes) state.likes = new Likes();
+	const currentID = state.recipe.id;
+
+	// User has NOT yet liked current recipe
+	if (!state.likes.isLiked(currentID)) {
+		// Add like to the state
+		const newLike = state.likes.addLike(currentID, state.recipe.title, state.recipe.author, state.recipe.img);
+		// Toggle the like button
+		likesView.toggleLikeBtn(true);
+		// Add like to the UI list
+		likesView.renderLike(newLike);
+
+		// User has liked current recipe
+	} else {
+		// Remove like from the state
+		state.likes.deleteLike(currentID);
+		// Toggle the like button
+		likesView.toggleLikeBtn(false);
+
+		// Remove like from UI list
+		likesView.deleteLike(currentID);
+		console.log(state.likes);
+	}
+	likesView.toggleLikeMenu(state.likes.getNumLikes());
+};
+
+// Restore liked recipes on page load
+window.addEventListener('load', () => {
+	state.likes = new Likes();
+
+	// Restore likes
+	state.likes.readStorage();
+
+	// Toggle like menu button
+	likesView.toggleLikeMenu(state.likes.getNumLikes());
+
+	// Render the existing likes
+	state.likes.likes.forEach((like) => likesView.renderLike(like));
+});
+
 // Handling recipe button clicks
 elements.recipe.addEventListener('click', (e) => {
 	if (e.target.matches('.btn-decrease, .btn-decrease *')) {
@@ -150,8 +194,10 @@ elements.recipe.addEventListener('click', (e) => {
 		state.recipe.updateServings('inc');
 		recipeView.updateServingsIngredients(state.recipe);
 	} else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
+		// Add ingredients to shopping list
 		controlList();
+	} else if (e.target.matches('.recipe__love, .recipe__love *')) {
+		// Like controller
+		controlLike();
 	}
 });
-
-window.l = new List();
